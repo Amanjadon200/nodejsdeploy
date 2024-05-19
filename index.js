1f674c5d043b8506f6089d9b4705a69d9d4eb826
Amanjadon200 <jadonaman471@gmail.com>
2024-05-19 16:57:12 +0530

    console.log

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var http = require('http');
 
 //create a server object:
 http.createServer(function (req, res) {
+  console.log("port is 80"); //con
   res.write('A Monk in Cloud'); //write a response to the client
   res.end(); //end the response
 }).listen(80); //the server object listens on port 80
